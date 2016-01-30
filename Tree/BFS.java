121e9b229547f368bdec801341045a1f62a7883a
PengFTang <pengftang@gmail.com>
2016-01-29 20:02:31 -0600

    Update BFS.java

--- Tree/BFS.java
+++ Tree/BFS.java
@@ -3,7 +3,6 @@ package tree;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Queue;
 
 /**
@@ -14,44 +13,6 @@ import java.util.Queue;
  */
 
 public class BFS<T> {
-	private static class TreeNode<T> {
-		public T val;
-	    public TreeNode<T> root=null, left=null, right=null;
-	    public TreeNode(T x) { 
-	    	val = x;
-	    }	
-	    // construct a binary tree with a formated string
-	    public TreeNode(String str) {
-	    	String[] strs = str.split(",");
-			int L = strs.length;
-			if(L < 1)  throw new NoSuchElementException("invalid input string");
-			
-			root = new TreeNode<>((T)convert(strs[0]));
-			Queue<TreeNode<T>> q = new LinkedList<>();
-			q.add(root);
-			int i = 0;
-			while(i<L-1) {
-				if(q.isEmpty()) throw new NoSuchElementException("invalid input string");
-				TreeNode<T> current = q.peek();
-				q.poll();
-				if(i<L-1 && !strs[++i].equals("")) {
-					current.left = new TreeNode<>((T)convert(strs[i]));
-					q.add(current.left);
-				}
-				if(i<L-1 && !strs[++i].equals("")) {
-					current.right = new TreeNode<>((T)convert(strs[i]));
-					q.add(current.right);
-				}
-			}
-	    }
-	    
-	    private Object convert(String str) {
-	    	if(val instanceof Integer) return Integer.parseInt(str);
-	    	if(val instanceof Character) return str.charAt(0);
-	    	return str;
-	    }
-	}
-	
 	public List<List<T>> iterativeBFS(TreeNode<T> root) {
 		List<List<T>> res = new ArrayList<>();
 		if(root==null) return res;
@@ -110,16 +71,17 @@ public class BFS<T> {
 	}
 	
 	public static void main(String[] args) {
-		BFS<Character> bfs = new BFS<>();
+		BFS<String> bfs = new BFS<>();
 		
 		String str = "1,2,3,4,,6,7,8,9,,11,,13,,,,17,18,,19,,20";
 		
-		TreeNode<Character> root = new TreeNode<Character>(str).root;
+		TreeNode<String> node = new TreeNode<String>("");
+		TreeNode<String> root = node.create(str);
 		
-		List<List<Character>> res = bfs.recursiveBFS(root);
+		List<List<String>> res = bfs.recursiveBFS(root);
 		System.out.println(res);
 		
-		List<List<Character>> res2 = bfs.iterativeBFS(root);
+		List<List<String>> res2 = bfs.iterativeBFS(root);
 		System.out.println(res2);
  	}
 }
